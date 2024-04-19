038bd5bb5ebed79bdf17787837ed36841c57d67c
s.shtanko <s.shtanko@krista.ru>
2024-04-20 02:39:25 +0300

    Пока скелет проги и кусок кода для вывода графика

---
+++ Streebog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Numerics;
+
+namespace Streebog
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            new ConsoleApplication().Start();
+        }
+        
+    }
+}
