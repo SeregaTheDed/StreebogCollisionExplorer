8c02a2b8db87850e780f37a5cc87c73868e291fa
s.shtanko <s.shtanko@krista.ru>
2024-04-25 19:33:33 +0300

    РАБОТАЕТ

--- Streebog/ImageMeaningfulCollisionsExplorer.cs
+++ Streebog/ImageMeaningfulCollisionsExplorer.cs
@@ -1,6 +1,7 @@
 ﻿using StreebogCollisionExplorer.ExploreCollision;
 using StreebogCollisionExplorer.Streebog;
 using System.Collections;
+using System.Drawing;
 using System.Security.Policy;
 
 namespace StreebogCollisionExplorer
@@ -10,82 +11,32 @@ namespace StreebogCollisionExplorer
         private static Bitmap WriteMessageToBitmap(Bitmap bitmap, byte[] msg)
         {
             Bitmap result = (Bitmap) bitmap.Clone();
-            BitArray bitArrayMsg = new BitArray(msg);
 
-            IEnumerator enumerator = bitArrayMsg.GetEnumerator();
-            enumerator.MoveNext();//Пропускаем тип сообщения
             for (int i = 0; i < result.Height; i++)
             {
                 for (int j = 0; j < result.Width; j++)
                 {
-                    bool bit = (bool) enumerator.Current;
-                    enumerator.MoveNext();
+                    int resultIndex = bitmap.Width * i + j + 1;
+                    byte pixelRedByte = msg[resultIndex];
                     Color pixel = result.GetPixel(j, i);
-                    byte redByte = pixel.R;
-                    if (bit && redByte % 2 != 1) 
-                    { 
-                        redByte++; 
-                    }
-                    else if (!bit && redByte % 2 != 0) 
-                    { 
-                        redByte--; 
-                    }
-
-                    result.SetPixel(j, i, Color.FromArgb(redByte, pixel.G, pixel.B));
+                    result.SetPixel(j, i, Color.FromArgb(pixelRedByte, pixel.G, pixel.B));
                 }
             }
             return result;
         }
-
-        private static BitArray GetBytesByBitmap(Bitmap bitmap, bool type)
+        public static byte[] GetMessageByBitmap(Bitmap bitmap, byte type)
         {
-            if (bitmap.Width * bitmap.Height % 8 != 7)
-            {
-                throw new ArgumentException("Ширина * Высоту картинки по модулю 8 должна быть == 7");
-            }
-            BitArray result = new BitArray(bitmap.Width * bitmap.Height + 1); //В начале у нас будет тип изображения
+            byte[] result = new byte[bitmap.Width * bitmap.Height + 1];
             result[0] = type;
             for (int i = 0; i < bitmap.Height; i++)
             {
                 for (int j = 0; j < bitmap.Width; j++)
                 {
                     int resultIndex = bitmap.Width * i + j + 1;
-                    bool pixelRedSmallByte = bitmap.GetPixel(j, i).R % 2 == 1;
-                    result[resultIndex] = pixelRedSmallByte;
+                    byte pixelRedByte = bitmap.GetPixel(j, i).R;
+                    result[resultIndex] = pixelRedByte;
                 }
             }
-            for (int i = 1; i < result.Length; i+=8)
-            {
-                /*for (int j = i; j < i + 10; j++)
-                {
-                    Console.Write(result[j] ? 1 : 0);
-                }
-                Console.WriteLine();*/
-                int right = Math.Min(i + 8, result.Length);
-                for (int j = 0; j < 4; j++)
-                {
-                    int index1 = j + i;
-                    int index2 = right-j-1;
-                    var temp = result[index1];
-                    result[index1] = result[index2];
-                    result[index2] = temp;
-                }
-                /*for (int j = i; j < i+10; j++)
-                {
-                    Console.Write(result[j] ? 1:0);
-                }
-                Console.WriteLine();
-                Console.WriteLine("---");*/
-
-            }
-            return result;
-        }
-        public static byte[] GetMessageByBitmap(Bitmap bitmap, bool type)
-        {
-            BitArray bitArray = GetBytesByBitmap(bitmap, type);
-            byte[] result = bitArray.ToBytes().ToArray();
-            //BitArray bitArray2 = new BitArray(result);
-            //byte[] result2 = bitArray2.ToBytes().ToArray();
             return result;
         }
 
@@ -94,8 +45,19 @@ namespace StreebogCollisionExplorer
             Bitmap bitmap1 = new Bitmap(Image.FromFile(imagePath1));
             Bitmap bitmap2 = new Bitmap(Image.FromFile(imagePath2));
 
-            byte[] msg1 = GetMessageByBitmap(bitmap1, false);
-            byte[] msg2 = GetMessageByBitmap(bitmap2, true);
+            int randomMessageSize = bitmap1.Width * bitmap1.Height + 1;
+            CollisionFinderResult collisionFinderResult =
+                new StandartCollisionFinder().FindCollisions(1, randomMessageSize);
+
+            byte type1 = collisionFinderResult.Messages.First().First();
+            byte type2 = collisionFinderResult.Messages.Last().First();
+            Console.WriteLine("Тип 1 = " + type1);
+            Console.WriteLine("Тип 2 = " + type2);
+
+
+
+            byte[] msg1 = GetMessageByBitmap(bitmap1, type1);
+            byte[] msg2 = GetMessageByBitmap(bitmap2, type2);
             if (msg1.Length != msg2.Length)
             {
                 throw new ArgumentException("Размеры картинок не совпадают");
@@ -111,25 +73,15 @@ namespace StreebogCollisionExplorer
                 $"{Convert.ToHexString(new StreebogAlgorithm().GetHash(msg2, 1))}" +
                 $"");
 
-            int randomMessageSize = msg1.Length;
-            CollisionFinderResult collisionFinderResult = 
-                new StandartCollisionFinder().FindCollisions(1, randomMessageSize);
-            while(collisionFinderResult.Messages.First().First() >> 7 == 1
-                ||
-                collisionFinderResult.Messages.Last().First() >> 7 == 0) 
-            {
-                // Находим такую коллизию, чтобы у нас различались типы сообщений (первый бит)
-                collisionFinderResult = new StandartCollisionFinder().FindCollisions(1, randomMessageSize);
-            }
-
+            
 
 
             //Записываем сообщения в картинки и сохраняем
             string collizedPrefix = "collized_";
-            WriteMessageToBitmap(bitmap1, collisionFinderResult.Messages.First())
-                .Save(collizedPrefix + "image1.png");
-            WriteMessageToBitmap(bitmap2, collisionFinderResult.Messages.Last())
-                .Save(collizedPrefix + "image2.png");
+            Bitmap bitmap1_tosave = WriteMessageToBitmap(bitmap1, collisionFinderResult.Messages.First());
+            bitmap1_tosave.Save(collizedPrefix + "image1.png");
+            Bitmap bitmap2_tosave = WriteMessageToBitmap(bitmap2, collisionFinderResult.Messages.Last());
+            bitmap2_tosave.Save(collizedPrefix + "image2.png");
             Console.WriteLine("Хеш найденной коллизии: " + collisionFinderResult.HashString);
 
             //Загружаем картинки, считаем в них хеши
@@ -138,8 +90,8 @@ namespace StreebogCollisionExplorer
             Bitmap bitmap1_collized = new Bitmap(Image.FromFile(image1_collized_path));
             Bitmap bitmap2_collized = new Bitmap(Image.FromFile(image2_collized_path));
 
-            byte[] msg1_collized = GetMessageByBitmap(bitmap1_collized, false);
-            byte[] msg2_collized = GetMessageByBitmap(bitmap2_collized, true);
+            byte[] msg1_collized = GetMessageByBitmap(bitmap1_collized, type1);
+            byte[] msg2_collized = GetMessageByBitmap(bitmap2_collized, type2);
 
             Console.WriteLine($"Хеш сообщения картинки {image1_collized_path} = " +
                 $"{Convert.ToHexString(new StreebogAlgorithm().GetHash(msg1_collized, 1))}" +
